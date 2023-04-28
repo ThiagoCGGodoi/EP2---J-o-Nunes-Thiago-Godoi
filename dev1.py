fb1de7f00a178c8c04b6746ee538821e05da2f1e
ThiagoCGGodoi <130405609+ThiagoCGGodoi@users.noreply.github.com>
2023-04-28 20:44:07 -0300

    EP2 - Preenche Frota

--- dev1.py
+++ dev1.py
@@ -7,3 +7,15 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
         for i in range(0,tamanho):
             posicoes.append([linha,coluna+i])
     return posicoes
+def preenche_frota(frota, nome, linha, coluna, orientacao, tamanho):
+    if len(frota) == 0:
+            frota[nome] = [define_posicoes(linha, coluna, orientacao, tamanho)]
+            return frota
+    for variavel1, variavel2 in frota.items():
+        if nome not in frota:
+            frota[nome] = [define_posicoes(linha, coluna, orientacao, tamanho)]
+            return frota
+        elif nome == variavel1:
+            variavel2.append(define_posicoes(linha, coluna, orientacao, tamanho))
+    return frota
+    
