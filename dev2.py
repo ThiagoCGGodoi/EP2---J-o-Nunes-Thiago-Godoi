5da6e5e1f3c7d4211a94b30c4fe163aa64242ede
João NUnes <joaovitor.ziliotto.ismart@gmail.com>
2023-05-02 17:49:08 -0300

    Posiciona Frota

--- dev2.py
+++ dev2.py
@@ -1,1 +1,9 @@
-print('João Nunes')
+def posiciona_frota(dicio):
+  '''tabuleiro = [[0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10]'''
+  for tipo_navio in dicio.values():
+    for navio in tipo_navio:
+      for coordenada in navio:
+        x = coordenada[0]
+        y = coordenada[1]
+        tabuleiro[x][y] = 1
+  return tabuleiro
